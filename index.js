b490a3943bee9e8a1d96431b4847c2050daba879
raymas <cSYvL6@yandex.com>
2019-07-08 21:22:24 +0200

    Adding tracker for tcp session

--- index.js
+++ index.js
@@ -2,14 +2,23 @@
 
 // Requirements
 var pcap = require('pcap'),
-    pcap_session = pcap.createSession('wlp4s0', '');
-var geoip = require('geoip-lite');
+    tcp_tracker = new pcap.TCPTracker(),
+    pcap_session = pcap.createSession('wlp4s0', ''),
+    geoip = require('geoip-lite');
+
+
+
+// TCP capture
+tcp_tracker.on('session', function (session) {
+    console.log("Start of session between " + session.src_name + " and " + session.dst_name);
+    session.on('end', function (session) {
+        console.log("End of TCP session between " + session.src_name + " and " + session.dst_name);
+    });
+});  
 
 
 // Packet capture is evenement based
 pcap_session.on('packet', function (raw_packet) {
     // do some stuff with a raw packet
-    var ip = pcap.decode.packet(raw_packet).payload.dhost.addr,
-        geo = geoip.lookup(ip.join('.'));
-    console.log(geo, ip.join('.'));
+    var packet  = pcap.decode.packet(raw_packet);
 });
